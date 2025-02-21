d4f017078dd33c2deb0a3a95f41db4ccad8e4348
Armin Meybodi <arminmeybodis@gmail.com>
2025-02-21 17:33:12 +0330

    initial commit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.l1 = new System.Windows.Forms.Label();
+            this.l = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.NumericUpDown();
+            this.txtNumber2 = new System.Windows.Forms.NumericUpDown();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMultiple = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumber1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumber2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Location = new System.Drawing.Point(654, 89);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(43, 13);
+            this.l1.TabIndex = 0;
+            this.l1.Text = "عدد اول";
+            this.l1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // l
+            // 
+            this.l.AutoSize = true;
+            this.l.Location = new System.Drawing.Point(654, 142);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(45, 13);
+            this.l.TabIndex = 1;
+            this.l.Text = "عدد دوم";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(271, 81);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(362, 21);
+            this.txtNumber1.TabIndex = 2;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(271, 134);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(362, 21);
+            this.txtNumber2.TabIndex = 3;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(211, 206);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(75, 23);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(342, 206);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(75, 23);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMultiple
+            // 
+            this.btnMultiple.Location = new System.Drawing.Point(487, 206);
+            this.btnMultiple.Name = "btnMultiple";
+            this.btnMultiple.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiple.TabIndex = 6;
+            this.btnMultiple.Text = "*";
+            this.btnMultiple.UseVisualStyleBackColor = true;
+            this.btnMultiple.Click += new System.EventHandler(this.btnMultiple_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(613, 206);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(739, 267);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiple);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.l);
+            this.Controls.Add(this.l1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeftLayout = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumber1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtNumber2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l;
+        private System.Windows.Forms.NumericUpDown txtNumber1;
+        private System.Windows.Forms.NumericUpDown txtNumber2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMultiple;
+        private System.Windows.Forms.Button btnDivide;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,14 +13,78 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
+        ICalculate calculate;
         public Form1()
         {
             InitializeComponent();
+            calculate = new Calculate();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int sum = calculate.Plus((int)txtNumber1.Value, (int)txtNumber2.Value);
+                MessageBox.Show("Sum Is : " + sum);
+            }
+        }
+
+        bool ValidateInputs()
+        {
+            bool isValid = true;
+
+            if (txtNumber1.Value == 0)
+            {
+                isValid = false;
+                MessageBox.Show("لطفا عدد اول را واردکنید");
+            }
+            else
+            {
+                if (txtNumber2.Value == 0)
+                {
+                    isValid = false;
+                    MessageBox.Show("لطفا عدد دوم را واردکنید");
+                }
+            }
+
+            return isValid;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int Minus = calculate.Minus(int.Parse(txtNumber1.Value.ToString()), int.Parse(txtNumber2.Value.ToString()));
+                MessageBox.Show("Minus is : " + Minus);
+            }
+        }
+
+        private void btnMultiple_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int multiple = calculate.Multiple((int)txtNumber1.Value, (int)txtNumber2.Value);
+                MessageBox.Show("Multiple Is : " + multiple);
+            }
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            if (ValidateInputs())
+            {
+                int divide = calculate.Divide((int)txtNumber1.Value, (int)txtNumber2.Value);
+                MessageBox.Show("Divide Is : " + divide);
+            }
         }
     }
 }
